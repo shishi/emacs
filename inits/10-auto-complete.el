36457a9271d9e01aaa7496e15375200253aa1b97
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2015-11-30 18:30:50 +0900

    update auto-complete

--- inits/10-auto-complete.el
+++ inits/10-auto-complete.el
@@ -1,6 +1,5 @@
 ;; auto-complete
 (require 'pos-tip)
-(require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 
@@ -12,12 +11,11 @@
 (setq ac-delay 0.3)
 (setq ac-use-quick-help t)
 (setq ac-quick-help-delay 0.3)
-
+;; 補完メニュー表示時にC-n/C-pで補完候補選択
 (setq ac-use-menu-map t)
+(setq ac-use-fuzzy t)
+
 (define-key ac-mode-map (kbd "M-RET") 'auto-complete)
-(define-key ac-completing-map (kbd "C-n") 'ac-next)
-(define-key ac-completing-map (kbd "C-p") 'ac-previous)
-(define-key ac-completing-map (kbd "C-m") 'ac-complete)
 
 ;; 日本語入力中は off
 (defadvice ac-on-post-command
