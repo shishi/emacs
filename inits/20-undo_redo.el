8f36e9caac08ddd75409c9ec67fa3f0ed5359572
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2015-12-01 10:33:35 +0900

    change redo keymap

--- inits/20-undo_redo.el
+++ inits/20-undo_redo.el
@@ -1,5 +1,5 @@
 ;;; redo+.el
 (require 'redo+)
 (define-key global-map (kbd "C-/") 'undo)
-(define-key global-map (kbd "C-x C-/") 'redo)
+(define-key global-map (kbd "C-M-/") 'redo)
 ;; see (find-file-other-window "20-smartrep.el")
