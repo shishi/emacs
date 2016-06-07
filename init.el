46a15ce605cf0c27cfb333d32234e6a89e5ced7d
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2016-06-07 11:29:43 +0900

    fix init.el

--- init.el
+++ init.el
@@ -23,28 +23,4 @@
 ;; init-loader
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
-;; (init-loader-show-log-after-init nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "c697b65591ba1fdda42fae093563867a95046466285459bd4e686dc95a819310" "045251e7ff119a8b065b4cb0072067eb2f297acc44a9e36407e6ff165e35c528" default)))
- '(js2-auto-indent-p t)
- '(js2-enter-indents-newline t)
- '(js2-indent-on-enter-key nil)
- '(rst-level-face-base-light 50)
- '(ruby-insert-encoding-magic-comment nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(rst-level-1-face ((t (:foreground "LightSkyBlue"))) t)
- '(rst-level-2-face ((t (:foreground "LightGoldenrod"))) t)
- '(rst-level-3-face ((t (:foreground "Cyan1"))) t)
- '(rst-level-4-face ((t (:foreground "chocolate1"))) t)
- '(rst-level-5-face ((t (:foreground "PaleGreen"))) t)
- '(rst-level-6-face ((t (:foreground "Aquamarine"))) t))
+;; (init-loader-show-log-after-init t)
