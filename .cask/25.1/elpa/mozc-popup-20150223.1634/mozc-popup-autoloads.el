3229002cecb3778566a298ddcee84f23571faf17
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2017-04-21 06:32:32 +0900

    update packages

---
+++ .cask/25.1/elpa/mozc-popup-20150223.1634/mozc-popup-autoloads.el
@@ -0,0 +1,15 @@
+;;; mozc-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("mozc-popup.el") (22777 9977 484244 181000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; mozc-popup-autoloads.el ends here
