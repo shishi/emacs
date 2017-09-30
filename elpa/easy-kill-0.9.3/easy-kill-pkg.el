c38677b56b9e0fce2426891fabe1bdc09f42fbd0
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2017-09-29 22:04:02 -0700

    remove non need files

--- elpa/easy-kill-0.9.3/easy-kill-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from easy-kill.el
-(define-package "easy-kill" "0.9.3" "kill & mark things easily" '((emacs "24") (cl-lib "0.5")) :url "https://github.com/leoliu/easy-kill" :keywords '("killing" "convenience"))
